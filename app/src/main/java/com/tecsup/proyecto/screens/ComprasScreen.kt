bc13e25385f251e0405dd0a380bb60f709f2495b
Abel23005 <imer.quispe@tecsup.edu.pe>
2025-11-01 23:14:01 -0500

    cambios

--- app/src/main/java/com/tecsup/proyecto/screens/ComprasScreen.kt
+++ app/src/main/java/com/tecsup/proyecto/screens/ComprasScreen.kt
@@ -303,7 +303,7 @@ fun ComprasContent(
             shape = RoundedCornerShape(8.dp)
         ) {
             Text(
-                text = "Guardar compra",
+                text = "Guardar compra ✅",
                 fontSize = 16.sp,
                 fontWeight = FontWeight.SemiBold
             )
